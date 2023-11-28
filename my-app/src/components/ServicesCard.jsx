eaf24a2d3157004f0df02f1d62ca99eb9e851eb6
Adev2598 <ammar.react.dev@outlook.com>
2023-11-28 15:57:39 +0400

    mods to swiper js

--- my-app/src/components/ServicesCard.jsx
+++ my-app/src/components/ServicesCard.jsx
@@ -1,56 +1,61 @@
 import React from 'react'
-import '../styles/ServicesCard.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/free-mode'
+// import '../styles/ServicesCard.css'
+
+import { FreeMode, Pagination, Autoplay } from 'swiper/modules'
+
+import { RxArrowTopRight} from 'react-icons/rx'
+import { ServiceData } from '../constants'
 
 const ServicesCard = () => {
+
+    
   return (
     <>
-            <h1 className='text-gray-800 text-center text-4xl pb-12 font-bold tracking-tight sm:text-6xl'>Our Services</h1>
-    <div className='bg-[#103025] px-56'>
-        <div className="wrapper">
-           <div className="container">
-                <input type="radio" name="slide" id="c1" checked />
-                <label htmlFor="c1" className='card'>
-                    <div className="row">
-                        <div className="icon">1</div>
-                        <div className="description">
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet</p>
+        <div className="flex items-center justify-center flex-col h-screen bg-[#103025]">
+            <h1 className='text-2xl text-white -mt-32 pb-14'> Our Services</h1>
+            <Swiper
+            autoplay={{ delay: 3000 }}
+            breakpoints={{
+                340: {
+                    slidesPreview:2,
+                    spaceBetween: 15
+                },
+                700: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                }
+            }}
+
+            freeMode={true}
+            pagination={{
+                clickable: true
+            }}
+            modules = {[FreeMode, Pagination, Autoplay]}
+            className='max-w-[90%] lg:max-w-[80%]'
+            >
+                {ServiceData.map((item) => (
+                    <SwiperSlide key={item.title}>
+                        <div className="flex flex-col gap-6 group relative shadow-lg 
+                        text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] 
+                        overflow-hidden cursor-pointer">
+                            <div className="absolute inset-0 bg-cover bg-center opacity-80" style={{backgroundImage: `url(${item.backgroundImage})`}}
+                            />
+                            <div className="absolute inset-0 opacity-10 bg-black group-hover:opacity-50"/>
+                            <div className="relative flex flex-col gap-3">
+                                <item.icon className='text-white group-hover:text-[#BC9442] w-[32px] h-[32px]'/>
+                                <h1 className="text-xl lg:text-2xl">{item.title}</h1>
+                                <p className='lg:text-[18px]'>{item.content}</p>
+                            </div> 
+                            <RxArrowTopRight className='absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-[#BC9442] group-hover:rotate-45 duration-100'/>                           
                         </div>
-                    </div>
-                </label>
-                <input type="radio" name="slide" id="c2" checked />
-                <label htmlFor="c2" className='card'>
-                    <div className="row">
-                        <div className="icon">2</div>
-                        <div className="description">
-                            <h4>Lorem ipsum </h4>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                </label>
-                <input type="radio" name="slide" id="c3" checked />
-                <label htmlFor="c3" className='card'>
-                    <div className="row">
-                        <div className="icon">3</div>
-                        <div className="description">
-                            <h4>Lorem ipsum </h4>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                </label>
-                <input type="radio" name="slide" id="c4" checked />
-                <label htmlFor="c4" className='card'>
-                    <div className="row">
-                        <div className="icon">4</div>
-                        <div className="description">
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                </label>
-            </div>  
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
-    </div>
     </>
   )
 }
---
+++ my-app/src/constants/index.js
@@ -0,0 +1,55 @@
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+} from "react-icons/rx";
+
+import ceramic1 from "../assets/images/ceramic1.png";
+import ceramic2 from "../assets/images/ceramic2.png";
+import ceramic3 from "../assets/images/ceramic3.png";
+
+import interior1 from "../assets/images/interior1.png";
+import interior2 from "../assets/images/interior2.png";
+import interior3 from "../assets/images/interior3.png";
+
+export const ServiceData = [
+  {
+    icon: RxCrop,
+    title: "Development",
+    content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+    backgroundImage: ceramic1,
+  },
+  {
+    icon: RxPencil2,
+    title: "Branding",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: ceramic2,
+  },
+  {
+    icon: RxDesktop,
+    title: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: ceramic3,
+  },
+  {
+    icon: RxReader,
+    title: "Seo",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: interior1,
+  },
+  {
+    icon: RxAccessibility,
+    title: "Management",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: interior2,
+  },
+  {
+    icon: RxRocket,
+    title: "Production",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: interior3,
+  },
+];
